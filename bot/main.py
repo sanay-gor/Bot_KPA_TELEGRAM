1deb08a313e2c8f9fddef83c71acbec6e11f3ae2
Alexandr Viktorov <aviktorov@dev.vtb.ru>
2023-04-18 16:58:03 +0300

    1

--- bot/main.py
+++ bot/main.py
@@ -2,7 +2,7 @@ import logging
 import telegram
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-
+from responses import responses
 
 API_KEY = "6037731646:AAESrQjxr0Kf5O0NO8vyMX_ZI5JditpoAQg"
 
@@ -16,13 +16,13 @@ def handle_message(update: Update, _: CallbackContext):
     question = update.message.text.lower()
 
     if 'бухгалтерию' in question or 'бухгалтерия' in question or 'бухгалтерский' in question or 'финансы' in question or 'зп' in question or 'зарплата' in question or 'документы' in question:
-        answer = "Бухгалтерия отвечает за учет и анализ финансовых операций компании. Если у вас возникли вопросы по бухгалтерии, вы можете обратиться к своему руководителю или бухгалтеру компании."
+        answer = responses['accounting']
     elif 'отдел кадров' in question or 'кадры' in question or 'hr' in question or 'персонал' in question or 'найм' in question or 'увольнение' in question:
-        answer = "Отдел кадров занимается подбором, наймом и увольнением сотрудников компании, а также следит за их карьерным ростом и развитием. Если у вас возникли вопросы по отделу кадров, вы можете обратиться к своему руководителю или HR-менеджеру компании."
+        answer = responses['hr']
     elif 'привет' in question or 'здравствуйте' in question or 'добрый день' in question or 'добрый вечер' in question or 'доброе утро' in question:
-        answer = "ОООО ебешишь за рис как я?!? че хотел?"
+        answer = responses['greetings']
     else:
-        answer = f"Вы задали вопрос: {question}. Ваш ответ: ..."
+        answer = responses['default'].format(question)
 
     update.message.reply_text(answer)
 
