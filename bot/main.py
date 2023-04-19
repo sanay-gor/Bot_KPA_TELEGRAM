a1cdfc9db3a1c0c15b2b86620635a382d7872c18
a.viktorov <a.viktorov@avtomatika.kz>
2023-04-19 13:49:47 +0600

    up

--- bot/main.py
+++ bot/main.py
@@ -1,73 +1,90 @@
-import logging
-import telegram
-import re
-from telegram import Update
+import os
+import glob
+from docx import Document
+import PyPDF2
+from telegram import Update, InputFile
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-from responses import responses
-from urllib.parse import quote
+from nextcloud import Nextcloud
+
+# Настройка токена и папки с документами
+TOKEN = "6214551617:AAGDXgiFtzZkoMaTxVrBXh2ESpSUvh9vyZc"
+DOCS_FOLDER = "docs/"
+
+# Настройка Nextcloud
+NEXTCLOUD_URL = "https://docs.avtomatika.kz/"
+NEXTCLOUD_USERNAME = "nexcloud"
+NEXTCLOUD_PASSWORD = "Hgiu0e4upfjsdhdksfht87LHUGFUFU232432!!"
+NEXTCLOUD_UPLOAD_FOLDER = "/var/snap/nextcloud/common/nextcloud/data/nexcloud
+
+nextcloud_client = Nextcloud(NEXTCLOUD_URL, NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD)
 
 
-API_KEY = "6214551617:AAGDXgiFtzZkoMaTxVrBXh2ESpSUvh9vyZc"
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-def start(update: Update, _: CallbackContext):
-    update.message.reply_text("Привет! Я бот, который отвечает на вопросы сотрудников. Задайте свой вопрос.")
-
-def handle_message(update: Update, _: CallbackContext):
-    question = update.message.text.lower()
-
-    accounting_keywords = r'\b(бухгалтери[яюе]|зп|бухгалтерск(ий|ой|ого|ая|ое)|финанс(ов|ы)?|з[а]?рплат(а|у|е|ой|ы)?|документ[ыа]?|налог[аи]?|отч[её]т[ау]?|бюджет)\b'
-    hr_keywords = r'\b(отдел(а|е)? кадр(ов|ы)?|кадров(ый|ого|ом|ая|ое|ых)?|hr|персонал|найм|увольнен(ие|ия)?|трудоустро[йи]ство|трудов(ой|ая|ые)? договор|работник[аи]?|сотрудник[аи]?)\b'
-    greetings_keywords = r'\b(привет|здравствуйте|добр(ый|ого) день|добр(ый|ого) вечер|доброе утро)\b'
-    it_keywords = r'\b(ошибк[а-я]*|сбо[йи]*|неполадк[а-я]*|не\s*работа[ею][тт]*|загрузк[а-я]*|установк[а-я]*|активаци[я-я]*|логин[а-я]*|парол[ьяи]*|вирус[а-я]*|безопасност[ьи]*|сетев[ооеё]*\s*подключен[иеия]*|браузер[а-я]*|(электронн[а-я]*\s*)?почт[а-я]*|синхронизац[ия-я]*|резервн[а-я]*\s*копирован[иеия]*|восстановлен[иеия-я]*\s*данных|настройк[а-я]*)\b'
-    sales_keywords = r'\b(продаж[и]?|отдел продаж|менеджер по продажам|клиент|контракт|сделк[аи])\b'
-    marketing_keywords = r'\b(маркетинг|реклам[аы]|бренд|продвижение|smm|seo|контент|социальн[ыа]е? меди[а]?|кампани[яи]?)\b'
-    development_keywords = r'\b(разработк[аи]|инженер[ы]?|программист|код|дизайнер|тестировщик|devops|frontend|backend|fullstack)\b'
-    project_management_keywords = r'\b(управление проектами|менеджер проекта|планирование|scrum|график|задач[аи]?|контроль|агил[е]?|методология)\b'
-    legal_keywords = r'\b(юридический отдел|адвокат|юрист|закон|право|договор|суд|спор|регистрация|лиценз[ияи]?)\b'
+def search_docx(keyword: str):
+    results = []
+    for doc_path in glob.glob(os.path.join(DOCS_FOLDER, "*.docx")):
+        doc = Document(doc_path)
+        for paragraph in doc.paragraphs:
+            if keyword in paragraph.text:
+                results.append(doc_path)
+                break
+    return results
 
 
-    if re.search(accounting_keywords, question, re.IGNORECASE):
-        answer = responses['accounting']
-    elif re.search(hr_keywords, question, re.IGNORECASE):
-        answer = responses['hr']
-    elif re.search(greetings_keywords, question, re.IGNORECASE):
-        answer = responses['greetings']
-    elif re.search(it_keywords, question, re.IGNORECASE):
-        encoded_question = quote(question)
-        yandex_search_url = f"https://yandex.ru/search/?text={encoded_question}"
-        answer = responses['it'] + f"\nДополнительно вы можете поискать информацию по этому запросу в Яндекс: {yandex_search_url}"
-    elif re.search(sales_keywords, question, re.IGNORECASE):
-        answer = responses['sales']
-    elif re.search(marketing_keywords, question, re.IGNORECASE):
-        answer = responses['marketing']
-    elif re.search(development_keywords, question, re.IGNORECASE):
-        answer = responses['development']
-    elif re.search(project_management_keywords, question, re.IGNORECASE):
-        answer = responses['project_management']
-    elif re.search(legal_keywords, question, re.IGNORECASE):
-        answer = responses['legal']
+def search_pdf(keyword: str):
+    results = []
+    for pdf_path in glob.glob(os.path.join(DOCS_FOLDER, "*.pdf")):
+        pdf = PyPDF2.PdfFileReader(pdf_path)
+        for page_num in range(pdf.numPages):
+            text = pdf.getPage(page_num).extractText()
+            if keyword in text:
+                results.append(pdf_path)
+                break
+    return results
+
+
+def upload_to_nextcloud_and_get_link(file_path: str):
+    with open(file_path, "rb") as file:
+        file_content = file.read()
+    file_name = os.path.basename(file_path)
+
+    # Загрузка файла в Nextcloud
+    response = nextcloud_client.upload_file(file_content, NEXTCLOUD_UPLOAD_FOLDER + "/" + file_name)
+
+    if response.is_ok:
+        # Создание публичной ссылки
+        share_response = nextcloud_client.create_share(NEXTCLOUD_UPLOAD_FOLDER + "/" + file_name, share_type=3)
+
+        if share_response.is_ok:
+            return share_response.get_link()
+        else:
+            raise Exception("Ошибка создания публичной ссылки: " + share_response.raw_error)
     else:
-        answer = responses['error'].format(question)
+        raise Exception("Ошибка загрузки файла: " + response.raw_error)
 
-    update.message.reply_text(answer)
 
-def error(update: Update, context: CallbackContext):
-    logger.error('Update "%s" caused error "%s"', update, context.error)
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("Введите ключевое слово для поиска в документах:")
+
+
+def search(update: Update, context: CallbackContext):
+    keyword = update.message.text
+
+    docx_results = search_docx(keyword)
+    pdf_results = search_pdf(keyword)
+
+    if not docx_results and not pdf_results:
+        update.message.reply_text("Документы, содержащие указанное ключевое слово, не найдены.")
+    else:
+        update.message.reply_text("Найденные документы:")
+        for result in docx_results + pdf_results:
+            try:
+                public_link = upload_to_nextcloud_and_get_link(result)
+                update.message.reply_text(f"Публичная ссылка: {public_link}")
+            except Exception as e:
+                update.message.reply_text(f"Ошибка загрузки файла {result} в Nextcloud: {str(e)}")
+
 
 def main():
-    bot = telegram.Bot(token=API_KEY)
-    updater = Updater(bot.token, persistence=None, workers=10)
+    updater = Updater(TOKEN)
 
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
-    dispatcher.add_error_handler(error)
-
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+    dp
